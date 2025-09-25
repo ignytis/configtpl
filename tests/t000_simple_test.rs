a6c32f21aecffa74f147ca647a0796ef9d188614
Ignytis <155588001+ignytis@users.noreply.github.com>
2025-09-26 01:12:13 +0300

    Improved the integration test

---
+++ tests/t000_simple_test.rs
@@ -0,0 +1,27 @@
+extern crate configtpl;
+
+use std::collections::HashMap;
+
+use configtpl::{config_builder::ConfigBuilder, types::config_param::ConfigParam};
+
+
+
+#[test]
+fn test_builder_simple() {
+    let builder = ConfigBuilder::new();
+
+    let mut cfg_urls: HashMap<String, ConfigParam> = HashMap::new();
+    cfg_urls.insert(String::from("base"), ConfigParam::String(String::from("example.com")));
+    cfg_urls.insert(String::from("mail"), ConfigParam::String(String::from("mail.example.com")));
+
+    let mut cfg_server: HashMap<String, ConfigParam> = HashMap::new();
+    cfg_server.insert(String::from("host"), ConfigParam::String(String::from("example.com")));
+    cfg_server.insert(String::from("port"), ConfigParam::Int(1234));
+
+
+    let mut cfg: HashMap<String, ConfigParam> = HashMap::new();
+    cfg.insert(String::from("urls"), ConfigParam::HashMap(cfg_urls));
+    cfg.insert(String::from("server"), ConfigParam::HashMap(cfg_server));
+
+    assert_eq!(cfg, builder.build_from_files(&String::from("tests/t000_simple/config.cfg"), &None, &None).unwrap());
+}
